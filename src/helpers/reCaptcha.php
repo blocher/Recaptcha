d1c17b711f0f2e3efe63344372e1bac64a53b4b6
Craig Russell <tenowg@gmail.com>
2014-05-13 19:21:58 -0400

    Getting helpers to work

--- src/helpers/reCaptcha.php
+++ src/helpers/reCaptcha.php
@@ -2,7 +2,7 @@
 
 class reCaptcha {
     public static function display() {
-        require_once(__DIR__.'../recaptchalib.php');
+        require_once(__DIR__.'/../recaptchalib.php');
         $publicKey = 'some public key';
         echo recaptcha_get_html($publicKey);
     }
