03f9eeecd0de2c0e5c9aad86423d30982555a84d
tenowg <tenowg@gmail.com>
2014-05-14 00:05:51 +0200

    inital

---
+++ src/Tenowg/Recapchta/RecapchtaServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Tenowg\Recapchta;
+
+use Illuminate\Support\ServiceProvider;
+
+class RecapchtaServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('tenowg/recapchta');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
