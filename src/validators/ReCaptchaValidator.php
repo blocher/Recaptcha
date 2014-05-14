42619cd5a6dfecb7fd0f6f569ec3c27c95ba1ff5
Craig Russell <tenowg@gmail.com>
2014-05-13 20:35:02 -0400

    Getting helpers to work

--- src/validators/ReCaptchaValidator.php
+++ src/validators/ReCaptchaValidator.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Translation\Translator;
+
 class ReCaptchaValidator extends Illuminate\Validation\Validator {
 
-    public function __construct(TranslatorInterface $translator, $data, $rules, $messages = array()) {
+    public function __construct(Translator $translator, $data, $rules, $messages = array()) {
         $messages = \Lang::get('Recaptcha::recaptcha');
 
         parent::__construct($translator, $data, $rules, $messages);
