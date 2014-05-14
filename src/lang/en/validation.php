db8dbca0d02fe43962ebb49cec944c36de8ad9da
Craig Russell <tenowg@gmail.com>
2014-05-13 20:32:37 -0400

    Getting helpers to work

---
+++ src/lang/en/validation.php
@@ -0,0 +1,6 @@
+<?php
+    return array(
+        'recaptcha' => array(
+            'failed'    => 'Recaptcha Failed'
+        )
+    );
--- src/validators/ReCaptchaValidator.php
+++ src/validators/ReCaptchaValidator.php
@@ -1,8 +1,16 @@
 <?php
 
 class ReCaptchaValidator extends Illuminate\Validation\Validator {
+
+    public function __construct(TranslatorInterface $translator, $data, $rules, $messages = array()) {
+        $messages = \Lang::get('Recaptcha::recaptcha');
+
+        parent::__construct($translator, $data, $rules, $messages);
+    }
+
     public function validateRecaptcha($attribute, $value, $parameters)
     {
-        return $value == 'foo';
+        echo $attribute . '  ' . $value . '  ' . $parameters;
+        return false;
     }
 } 
