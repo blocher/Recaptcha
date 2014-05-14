da3fba4da63469275b441892f53af66bddfcd94a
Craig Russell <tenowg@gmail.com>
2014-05-13 21:21:56 -0400

    Getting helpers to work

--- src/validators/ReCaptchaValidator.php
+++ src/validators/ReCaptchaValidator.php
@@ -15,7 +15,7 @@ class ReCaptchaValidator extends Illuminate\Validation\Validator {
         require_once(__DIR__.'/../recaptchalib.php');
         $privateKey = \Config::get('Recaptcha::privatekey');
 
-        $response = recaptcha_check_answer($privateKey, Request::getClientIp(), $value, $parameters[0]);
+        $response = recaptcha_check_answer($privateKey, Request::getClientIp(), $parameters[0], $value);
 var_dump($parameters);
         if (!$response->is_valid) {
             return false;
