c0f09553c98837946a5b93139005763099a734ce
Benjamin Locher <blocher@gmail.com>
2017-11-01 17:36:54 -0400

    forcing ssl

--- src/helpers/reCaptcha.php
+++ src/helpers/reCaptcha.php
@@ -4,6 +4,6 @@ class reCaptcha {
     public static function display() {
         require_once(__DIR__.'/../recaptchalib.php');
         $publicKey = \Config::get('Recaptcha::publickey');
-        echo recaptcha_get_html($publicKey);
+        echo recaptcha_get_html($publicKey, null, true);
     }
-} 
+}
